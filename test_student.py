7c87f8764d5fda247ff522de9905b5061fa7f70d
Zacharia <zacharia.mesbah@insa-rouen.fr>
2020-01-23 11:20:04 +0100

    pytest

---
+++ test_student.py
@@ -0,0 +1,9 @@
+import pytest as pt
+import ../Student
+import main
+def test_set_get():
+    students = main.set_students_data()
+    for i in students:
+        name = 'foo', str(i)
+        i.set_prenom(name)
+        assert(i.get_prenom() == name)
